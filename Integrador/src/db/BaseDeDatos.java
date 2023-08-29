fd3195787efceb0466c88f8f5a4f94a59b287f4e
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2023-08-29 20:31:04 -0300

    Cambia los path para que sean relativos

--- Integrador/src/db/BaseDeDatos.java
+++ Integrador/src/db/BaseDeDatos.java
@@ -29,11 +29,11 @@ public class BaseDeDatos {
 
 		// Punto 2: Leer CSVs y cargar datos a la base de datos
 		
-		String path1 = "C:/eclipse-workspace/Clone 1/Arquitecturas-Web-Integrador/Integrador/clientes.csv";
-		String path2 = "C:/eclipse-workspace/Clone 1/Arquitecturas-Web-Integrador/Integrador/productos.csv";
-		String path3 = "C:/eclipse-workspace/Clone 1/Arquitecturas-Web-Integrador/Integrador/facturas.csv";
-		String path4 = "C:/eclipse-workspace/Clone 1/Arquitecturas-Web-Integrador/Integrador/facturas-productos.csv";
-/*
+		String path1 = "src/csv/clientes.csv";
+		String path2 = "src/csv/productos.csv";
+		String path3 = "src/csv/facturas.csv";
+		String path4 = "src/csv/facturas-productos.csv";
+
 		cliente_dao.createTable();
 		cliente_dao.poblateTable(path1);
 
@@ -45,7 +45,7 @@ public class BaseDeDatos {
 
 		factura_producto_dao.createTable();
 		factura_producto_dao.poblateTable(path4);
-*/
+
 		// Punto 3: Obtener el producto que mas cantidades vendio
 		
 		System.out.println(producto_dao.moreRaisedProduct());
